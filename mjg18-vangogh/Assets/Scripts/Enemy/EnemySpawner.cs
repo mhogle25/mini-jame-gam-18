ef702a935cda7ec359d987b46425c79e7a5da128
Mitchell <mhogle@bizinta.com>
2023-06-03 19:18:35 -0700

    added enemy files

---
+++ mjg18-vangogh/Assets/Scripts/Enemy/EnemySpawner.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    private EnemyController enemyPrefab = null;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+}
